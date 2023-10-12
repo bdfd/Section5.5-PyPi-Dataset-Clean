677e5c565fd2e00c5764736885df9eb7c20a0e65
David Chen <bdfd2005@gmail.com>
2023-10-12 10:35:16 -0400

    update two new function
    filtered_value_count and train_split

--- execdata/standardization.py
+++ execdata/standardization.py
@@ -2,7 +2,7 @@
 Date         : 2022-10-25 17:21:52
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-10-11 16:11:24
+LastEditTime : 2023-10-12 10:32:46
 LastEditors  : BDFD
 Description  : 
 FilePath     : \execdata\standardization.py
@@ -15,12 +15,20 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 
+
 def encode(df):
     lable = LabelEncoder()
     for column in df:
         if df[column].dtypes == 'object':
             df[column] = lable.fit_transform(df[column])
     return df
+
+
+def split(df, test_size=0.2, random_state=66):
+    df_train, df_test = train_test_split(
+        df, test_size=test_size, random_state=random_state)
+    return df_train, df_test
+
 
 def sep(df_train, df_test, target_variable):
     X_train = df_train.drop(target_variable, axis=1)
@@ -29,10 +37,13 @@ def sep(df_train, df_test, target_variable):
     y_test = df_test[target_variable]
     return X_train, y_train, X_test, y_test
 
-def split(df, target_variable, test_size=0.2, random_state=66):
-    df_train, df_test = train_test_split(df, test_size=test_size, random_state=random_state)
+
+def train_split(df, target_variable, test_size=0.2, random_state=66):
+    df_train, df_test = train_test_split(
+        df, test_size=test_size, random_state=random_state)
     X_train, y_train, X_test, y_test = sep(df_train, df_test, target_variable)
     return X_train, y_train, X_test, y_test
+
 
 def strat_split(df, column):
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=66)
