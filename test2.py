2c53ae08f1c99095ddea1d70787be369928a9827
David Chen <bdfd2005@gmail.com>
2023-11-01 14:20:02 -0400

    add three new function
    1)sort_cateogrical_feature reorder the categorical features
    2)fit_label_encode use for store lable encode for later on usage
    3)transform_label_encode use the store label encode to transform the dataset
    4)inverse_label_encode inverse the dataset back to original

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@
 Date         : 2023-10-11 13:01:26
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-11-01 12:30:36
+LastEditTime : 2023-11-01 14:07:22
 LastEditors  : BDFD
 Description  : 
 FilePath     : \test2.py
@@ -30,6 +30,16 @@ df.drop(columns=['customerID'], inplace=True)
 # print(df.isnull().sum())
 cat_features = {feature for feature in df.columns if df[feature].dtypes == 'O'}
 # print(len(cat_features))
-feature_le = exe.fit_label_encode(df, cat_features)
-print(feature_le[0], 'with', feature_le[0].classes_)
-exe.transform_label_encode(df, cat_features, feature_le)
+print(df)
+new_cat_features = exe.sort_categorical_feature(cat_features)
+print(new_cat_features)
+feature_le = exe.fit_label_encode(df, new_cat_features)
+# print('0', cat_features[0])
+# print('1', feature_le[0], 'with', feature_le[0].classes_)
+# print('2', df['Churn'].unique())
+new_df = exe.transform_label_encode(df, new_cat_features, feature_le)
+# print(df)
+print(new_df)
+# print(df)
+inverse_df = exe.inverse_label_encode(new_df, new_cat_features, feature_le)
+print(inverse_df)
