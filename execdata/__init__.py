285bacdf0ba622d7702c06b6ef384959f6b4b654
David Chen <bdfd2005@gmail.com>
2023-11-02 18:07:35 -0400

    change folder structure, and major version update

--- execdata/__init__.py
+++ execdata/__init__.py
@@ -2,7 +2,7 @@
 Date         : 2022-10-25 15:44:41
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-11-02 17:09:41
+LastEditTime : 2023-11-02 18:01:07
 LastEditors  : BDFD
 Description  : 
 FilePath     : \execdata\__init__.py
@@ -10,10 +10,10 @@ Copyright (c) 2022 by BDFD, All Rights Reserved.
 '''
 
 from execdata import templateproj
-from execdata.data_conversion import convint, convfloat
-from execdata.data_mining import filtered_value_count, filtered_value_list, column_identify, majority_target_variable
-from execdata.data_preprocess import drop_columns, fit_label_encode, transform_label_encode, inverse_label_encode, sort_categorical_feature
-from execdata.standardization import sep, split, sep_split, strat_split
+from execdata.data_preprocessing.data_conversion import *
+from execdata.data_preprocessing.data_mining import *
+from execdata.data_preprocessing.data_preprocess import *
+from execdata.data_preprocessing.standardization import *
 # from execdata.standardization import encode
-from execdata.model_evaluate import sample_comparsion, model_evaluate, algo_accuracy, result_comparision
-from execdata.analysis_graph import data_analysis_graph
+from execdata.data_modeling.model_evaluate import *
+from .analysis_graph import data_analysis_graph
--- top_correlation.py
+++ top_correlation.py
@@ -2,7 +2,7 @@
 Date         : 2023-11-02 15:30:55
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-11-02 17:14:52
+LastEditTime : 2023-11-02 18:06:59
 LastEditors  : BDFD
 Description  : 
 FilePath     : \top_correlation.py
@@ -22,5 +22,6 @@ new_cat_features = exe.sort_categorical_feature(cat_features)
 feature_le = exe.fit_label_encode(df, new_cat_features)
 transformed_cat_df = exe.transform_label_encode(
     df, new_cat_features, feature_le)
-top_correlation_list = exe.data_analysis_graph.top_correlation(df, target_feature)
+top_correlation_list = exe.analysis_graph.top_correlation(
+    df, target_feature)
 print(top_correlation_list)
