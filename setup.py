4356ae1bb8cd56a3d960d2d1f5d4d969ee60e70f
David Chen <bdfd2005@gmail.com>
2023-11-02 16:48:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Date         : 2022-10-25 15:44:41
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-11-02 16:45:10
+LastEditTime : 2023-11-02 16:48:26
 LastEditors  : BDFD
 Description  : 
 FilePath     : \setup.py
@@ -15,7 +15,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = '2.2.7'
+VERSION = '2.2.8'
 DESCRIPTION = 'Preprocessing dataset'
 PACKAGE_NAME = 'execdata'
 
