aa0270fb3b51b1da56d1bab894bbd76a605308b2
David Chen <bdfd2005@gmail.com>
2023-11-03 11:04:20 -0400

    Update __init__.py

--- execdata/__init__.py
+++ execdata/__init__.py
@@ -2,7 +2,7 @@
 Date         : 2022-10-25 15:44:41
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-11-03 10:53:38
+LastEditTime : 2023-11-03 11:03:56
 LastEditors  : BDFD
 Description  : 
 FilePath     : \execdata\__init__.py
@@ -13,5 +13,5 @@ from execdata import templateproj
 from .data_preprocessing.data_conversion import *
 from .data_preprocessing import _data_mining, _data_preprocess, _standardization
 # from execdata.standardization import encode
-from data_modeling import _model_evaluate
+from .data_modeling import _model_evaluate
 from .analysis_graph import _data_analysis_graph
