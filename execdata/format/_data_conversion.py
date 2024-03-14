15a59c052ba6d1705d349be3ece9a8ce4d2e41f8
David Chen <bdfd2005@gmail.com>
2024-03-14 14:28:06 -0400

    update new function addten

--- execdata/format/_data_conversion.py
+++ execdata/format/_data_conversion.py
@@ -2,10 +2,10 @@
 Date         : 2023-01-17 10:33:06
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-01-17 16:00:00
-LastEditors  : BDFD
+LastEditTime : 2024-03-14 14:27:06
+LastEditors  : <BDFD>
 Description  : 
-FilePath     : \execdata\data_conv.py
+FilePath     : \execdata\format\_data_conversion.py
 Copyright (c) 2023 by BDFD, All Rights Reserved. 
 '''
 def convint(str):
@@ -16,4 +16,9 @@ def convint(str):
 def convfloat(str):
     result = None
     result = float(str)
+    return result
+
+def addten(x):
+    result = x + 10
+    result = int(result)
     return result
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 Date         : 2022-10-25 15:44:41
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-12-01 11:59:22
-LastEditors  : BDFD
+LastEditTime : 2024-03-14 14:27:40
+LastEditors  : <BDFD>
 Description  : 
 FilePath     : \setup.py
 Copyright (c) 2022 by BDFD, All Rights Reserved. 
@@ -15,7 +15,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = '5.2.0'
+VERSION = '5.2.1'
 DESCRIPTION = 'Preprocessing dataset'
 PACKAGE_NAME = 'execdata'
 
