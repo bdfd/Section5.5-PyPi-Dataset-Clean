829da24a25b34fdadfa7ce2636afb59e4565b671
David Chen <bdfd2005@gmail.com>
2024-04-05 12:18:21 -0400

    add new box_plot function

--- execdata/graph/__init__.py
+++ execdata/graph/__init__.py
@@ -2,7 +2,7 @@
 Date         : 2023-11-02 12:44:48
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2024-04-05 11:00:56
+LastEditTime : 2024-04-05 12:12:02
 LastEditors  : <BDFD>
 Description  : 
 FilePath     : \execdata\graph\__init__.py
@@ -10,7 +10,8 @@ Copyright (c) 2023 by BDFD, All Rights Reserved.
 '''
 
 from ._data_analysis_graph import top_correlation
-from ._data_analysis_graph import categorical_numerical_feature_vs_target_graph
+from ._data_analysis_graph import count_plot_graph
+from ._data_analysis_graph import box_plot_graph
 
 from ._data_mining_graph import missing_value_analysis
 
--- execdata/graph/_data_analysis_graph.py
+++ execdata/graph/_data_analysis_graph.py
@@ -2,7 +2,7 @@
 Date         : 2023-11-02 12:47:29
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2024-04-05 11:58:13
+LastEditTime : 2024-04-05 12:16:46
 LastEditors  : <BDFD>
 Description  : 
 FilePath     : \execdata\graph\_data_analysis_graph.py
@@ -50,8 +50,10 @@ def top_correlation(df, target_feature, top_feature_number=10, correlation_bound
 
     return top_feature_column_names
 
+# combine use with function[exe.data_mining.categorical_numerical_feature_list]
 
-def categorical_numerical_feature_vs_target_graph(df, categorical_numerical_feature_list, target_feature):
+
+def count_plot_graph(df, categorical_numerical_feature_list):
     figsize = math.ceil(math.sqrt(len(categorical_numerical_feature_list)))
     fig = plt.figure(figsize=(18, 15))
     gs = fig.add_gridspec(figsize, figsize)
@@ -78,4 +80,34 @@ def categorical_numerical_feature_vs_target_graph(df, categorical_numerical_feat
                 ax.set_xlabel(
                     categorical_numerical_feature_list[num], fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
                 ax.set_ylabel('Count Number', fontsize=14,
+                              fontweight='bold', fontfamily='serif', color="#000000")
+
+# combine use with function[exe.data_mining.continuous_numerical_feature_list]
+
+
+def box_plot_graph(df, continuous_numerical_feature_list):
+    figsize = math.ceil(math.sqrt(len(continuous_numerical_feature_list)))
+    fig = plt.figure(figsize=(18, 15))
+    gs = fig.add_gridspec(figsize, figsize)
+    gs.update(wspace=0.5, hspace=0.25)
+    for i in range(figsize):
+        for j in range(figsize):
+            num = 0
+            num = i * figsize + j
+            if num < len(continuous_numerical_feature_list):
+                ax = fig.add_subplot(gs[i, j])
+                background_color = "#ffe6e6"
+                color_palette = ["#800000", "#8000ff",
+                                 "#6aac90", "#5833ff", "#da8829"]
+                fig.patch.set_facecolor(background_color)
+                ax.set_facecolor(background_color)
+                ax.text(-0.05, 81, continuous_numerical_feature_list[num],
+                        fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+                ax.grid(color='#000000', linestyle=':',
+                        axis='y', zorder=0,  dashes=(1, 5))
+                sns.boxenplot(ax=ax, y=df[continuous_numerical_feature_list[num]], palette=[
+                              "#800000"], width=0.6)
+                ax.set_xlabel(
+                    continuous_numerical_feature_list[num], fontsize=14, fontweight='bold', fontfamily='serif', color="#000000")
+                ax.set_ylabel('Measure Value', fontsize=14,
                               fontweight='bold', fontfamily='serif', color="#000000")
